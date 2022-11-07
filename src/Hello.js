766bcc7005f8fbb60b7e7e97fcb6256caeeb521c
jay123son <youremail@domain.com>
2022-11-07 16:42:49 -0500

    hello world

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Hello(prop){
+    
+    return(
+        <div>
+            <h1>"Hello World"</h1>
+            <h1>"Hello" {prop.name}</h1>
+            </div>
+    )
+}
+
+
+
+export default Hello
